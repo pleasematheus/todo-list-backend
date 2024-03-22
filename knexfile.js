d0cc9edb73e9348c14e8055c57cbfd341efa5c1f
pleasematheus <matheus4k@gmail.com>
2024-03-22 19:52:45 -0400

    First commit

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: 'postgresql',
+  connection: {
+    database: 'tasks',
+    user:     'postgres',
+    password: '123'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
